a21424b9f6c872cf214b2af1f4f6fc6331659868
Miguel Targa <miguel@games.com>
2016-07-03 08:21:13 -0700

    Checks if PlaylisTV exists and ask to install

--- app/src/main/java/me/targa/demo/SplashActivity.java
+++ app/src/main/java/me/targa/demo/SplashActivity.java
@@ -1,7 +1,10 @@
 package me.targa.demo;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,12 +13,59 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent shortcutIntent = new Intent();
-        shortcutIntent.setClassName("me.targa.playlistvbeta", "me.targa.playlistvbeta.activity.CategoryActivity");
-        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        shortcutIntent.putExtra("url", "http://cinebox.ga");
-        startActivity(shortcutIntent);
-        finish();
+
+        boolean installed = appInstalledOrNot("me.targa.playlistvbeta");
+        if(installed) {
+            Intent shortcutIntent = new Intent();
+            shortcutIntent.setClassName("me.targa.playlistvbeta", "me.targa.playlistvbeta.activity.CategoryActivity");
+            shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            /*
+                Change the line bellow with your playlist URL
+             */
+            shortcutIntent.putExtra("url", "http://cinebox.ga");
+            startActivity(shortcutIntent);
+            finish();
+        }else{
+            installPlaylisTV();
+        }
+    }
+
+    //Credits: http://stackoverflow.com/a/11392276/1366991
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
+
+
+    public void installPlaylisTV(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(R.string.install_playlistv)
+        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=me.targa.playlistvbeta")));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=me.targa.playlistvbeta")));
+                }
+            }
+        })
+        .setNegativeButton(R.string.no,new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
